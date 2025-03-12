7b6ee53087eaab6398aef0eca9610424bce1e75c
JonathanM-37 <Jonathanmendezmartinez37@gmail.com>
2025-03-11 19:41:45 -0600

    actualizacion de correcciones

--- practica12_23270063.py
+++ practica12_23270063.py
@@ -10,7 +10,7 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="papu19",
+        password="password",
         database="practica05_usuarios"
     )
 
